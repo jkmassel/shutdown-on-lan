ad153b2e1a16c2c3a7dbbf0a9e79bbc39adf0705
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-12-30 13:13:08 -0700

    Bundle 32 and 64-bit vcredist
    Install x86 and x64 redist

--- .github/commands/build-for-windows.bat
+++ .github/commands/build-for-windows.bat
@@ -11,7 +11,12 @@ cd windows
 "%WIX%\bin\candle.exe" -ext WixFirewallExtension -ext WixUtilExtension -ext WixBalExtension Product.wxs
 "%WIX%\bin\light.exe" -ext WixFirewallExtension -ext WixUtilExtension -ext WixBalExtension Product.wixobj
 
-ls
+echo "Downloading vcredist 2015"
+
+curl https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x86.exe --output vc_redist.x86.exe
+curl https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe --output vc_redist.x64.exe
+
+echo "Packaging Installer"
 
 "%WIX%\bin\candle.exe" -ext WixFirewallExtension -ext WixUtilExtension -ext WixBalExtension Bundle.wxs
 "%WIX%\bin\light.exe" -ext WixFirewallExtension -ext WixUtilExtension -ext WixBalExtension Bundle.wixobj
